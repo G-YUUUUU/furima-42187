fa4967e8015e5347e7dce134d96b80c5d9961310
G-YUUUUU <mizu0719yu@gmail.com>
2025-08-13 15:07:48 +0900

    ユーザー管理機能_修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   ZENKAKU_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  KATAKANA_REGEX = /\A[ァ-ヶ]+\z/
+  KATAKANA_REGEX = /\A[ァ-ヶー]+\z/
   validates :nickname, presence: true
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :last_name, presence: true, format: { with: ZENKAKU_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
-    email                 { 'test@example.com' }
+    email                 { Faker::Internet.email }
     password              { 'AbCd1234' }
     password_confirmation { password }
     last_name              { '山田' }
@@ -11,15 +11,12 @@ FactoryBot.define do
     birth_date             { Faker::Date.between(from: '1930-01-01', to: '2020-12-31') }
 
     trait :another_user do
-      nickname              { Faker::Name.name }
-      email                 { 'Test@example.com' }
       password              { 'test1111' }
       password_confirmation { password }
       last_name              { '鈴木' }
       first_name             { '太郎' }
       last_name_kana         { 'スズキ' }
       first_name_kana        { 'タロウ' }
-      birth_date             { Faker::Date.between(from: '1930-01-01', to: '2020-12-31') }
     end
   end
 end
